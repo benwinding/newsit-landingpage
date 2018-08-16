9032d70be4cbfb86d01ac8aab4bb09cc0f1bf0a7
Ben Winding <ben.winding@gmail.com>
2018-08-16 11:37:39 +0930

    Better server

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000
+console.log("Listening on port: " + port)
+app.listen(port);
